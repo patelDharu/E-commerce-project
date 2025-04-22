2327397e74eb1a1d51f029826bd6971c205f8b12
Dharu <168706043+patelDharu@users.noreply.github.com>
2025-04-22 10:08:24 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -117,7 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -126,8 +127,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import os
 
-MEDIA_URL = 'image/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'/image/')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
