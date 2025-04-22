e821745423c2f0574013a65f46ac11ccbf654cee
Dharu <168706043+patelDharu@users.noreply.github.com>
2025-04-22 10:24:23 +0530

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('',include('ecom.urls'))
 ] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
 
-if settings.DEBUGS:
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_URL)
